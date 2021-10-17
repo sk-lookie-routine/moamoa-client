7900c41c818cbff55335e680d18a5ba693f4e64d
Yujin <yujinyll9148@gmail.com>
2021-10-17 22:00:47 +0900

    컴포넌트 추가 및 경로 변경
    페이지 관련된 컴포넌트 views 폴더 아래로 이동

---
+++ src/components/views/home/TheCarousel.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>슬라이더</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+	background-color: var(--gray04);
+	height: 48rem;
+}
+</style>
---
+++ src/components/views/studylist/SearchBar.vue
@@ -0,0 +1,19 @@
+<template>
+	<h2>필요한 스터디를 빠르게 찾아보세요!</h2>
+	<form action="submit">
+		<label for="search"><img src="@/assets/img/icon_search.svg" /></label>
+		<input type="text" name="search" placeholder="제목, 태그로 검색하세요." />
+	</form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+form {
+	border: 0.1rem solid var(--gray02);
+	padding: 2rem 1.3rem;
+	border-radius: 1rem;
+}
+</style>

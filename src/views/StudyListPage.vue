401e565d90f57f8b333f66e83a8bf7e4db9603e5
Yujin <yujinyll9148@gmail.com>
2021-10-17 22:01:43 +0900

    스터디룸 스터디 목록 페이지 추가

---
+++ src/views/StudyListPage.vue
@@ -0,0 +1,17 @@
+<template>
+	<main-header></main-header>
+	<div class="box-with-underline"><h1>스터디 목록</h1></div>
+	<search-bar></search-bar>
+	<base-tab></base-tab>
+	<base-card></base-card>
+</template>
+
+<script>
+import SearchBar from '@/components/views/studylist/SearchBar.vue';
+
+export default {
+	components: { SearchBar },
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/StudyRoomPage.vue
@@ -0,0 +1,9 @@
+<template>
+	<h1>스터디룸</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>

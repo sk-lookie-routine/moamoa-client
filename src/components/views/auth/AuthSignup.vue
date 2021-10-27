21b03956eaaee2a866d51ad5c61d05946d0974d3
leeseooo <leeseoooo@naver.com>
2021-10-27 20:52:35 +0900

    auth페이지들 css 수정

--- src/components/views/auth/AuthSignup.vue
+++ src/components/views/auth/AuthSignup.vue
@@ -1,16 +1,22 @@
 <template>
 	<div class="auth_form">
-		<div class="auth_text">소셜 계정으로 간편 회원가입</div>
-		<div class="auth_text_decoration"></div>
-		<div class="auth_buttons">
-			<button class="kbtn"><img src="@/assets/img/kakao_signup.svg" /></button>
-			<button class="gbtn"><img src="@/assets/img/google_signup.svg" /></button>
+		<div class="auth-text">소셜 계정으로 간편 회원가입</div>
+		<div class="box--underline"></div>
+		<div class="auth-buttons">
+			<button class="kakao-signup-btn">
+				<img src="@/assets/img/icon_kakao.svg" />
+				<div class="kakao-signup-btn-text">카카오 아이디로 가입하기</div>
+			</button>
+			<button class="google-signup-btn" @click="googleLoginBtn">
+				<img src="@/assets/img/icon_google.svg" />
+				<div class="google-signup-btn-text">구글 아이디로 가입하기</div>
+			</button>
 		</div>
-		<div class="auth_isMember">
+		<div class="auth-isMember">
 			이미 MOAMOA의 회원이신가요?
-			<router-link to="/login" class="auth_link">로그인</router-link>
+			<router-link to="/login" class="auth-link">로그인</router-link>
 		</div>
-		<div class="auth_etc">
+		<div class="auth-etc">
 			귀하는 MOAMOA의 이용약관과 개인정보처리방침을 읽고 이해했으며 그에
 			동의함을 확인합니다.
 		</div>
@@ -20,7 +26,13 @@
 <script></script>
 
 <style scoped>
-.auth_text {
+* {
+	font-family: noto-sans-cjk-kr, sans-serif;
+}
+.auth-form {
+	width: 100%;
+}
+.auth-text {
 	font-family: noto-sans-cjk-kr, sans-serif;
 	font-weight: 700;
 	font-style: normal;
@@ -29,29 +41,51 @@
 	margin: 0 auto;
 	padding-top: 16.6rem;
 	padding-bottom: 0.7rem;
-	/* margin: 16.6rem 62.3rem 0 62.2rem; */
 	width: 21.7rem;
 	height: 2.7rem;
-	color: #4e4e4e;
+	color: var(--black);
 }
-.auth_text_decoration {
-	border: 0.7px solid #d8d8d8;
-	background: #d8d8d8;
-	width: 50rem;
-	margin: 0 auto;
+.box--underline {
+	margin: 0 47rem;
+	margin-top: 3.3rem;
 }
-.auth_buttons {
+.auth-buttons {
 	margin: 0 auto;
-	/* margin: 6rem 57.2rem 7.8rem 57.6rem; */
 	display: flex;
 	flex-direction: column;
 }
-.kbtn {
+.kakao-signup-btn {
 	margin-top: 6rem;
 	margin-bottom: 1.2rem;
+	width: 29.2rem;
+	height: 4.4rem;
+	background: #fee500;
+	color: #3c1e1e;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
 }
-.gbtn {
+.kakao-signup-btn-text {
+	font-size: 1.2rem;
+	margin-right: 7.3rem;
+}
+.google-signup-btn-text {
+	font-size: 1.2rem;
+	margin-right: 7.8rem;
+}
+.kakao-signup-btn img,
+.google-signup-btn img {
+	border: 1px solid red;
+	margin-left: 1.2rem;
+}
+.google-signup-btn {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
 	margin-bottom: 9rem;
+	width: 29.2rem;
+	height: 4.4rem;
+	border: 1px solid black;
 }
 button {
 	background: none;
@@ -63,12 +97,11 @@ button {
 button:hover {
 	cursor: pointer;
 }
-.auth_isMember {
+.auth-isMember {
 	font-family: noto-sans-cjk-kr, sans-serif;
 	font-weight: 300;
 	font-style: normal;
 	display: flex;
-	flex-direction: row;
 	margin: 0 auto;
 	/* margin: 1.4rem 56.9rem 0 56.9rem; */
 	align-items: center;
@@ -76,22 +109,21 @@ button:hover {
 	line-height: 1.7rem;
 	width: 30.2rem;
 	height: 2.9rem;
-	background: #fbfbfb;
+	background: var(--gray06);
 	border-radius: 1rem;
 	justify-content: center;
-	color: #a1a1a1;
+	color: var(--gray02);
 }
-.auth_link {
+.auth-link {
 	padding-left: 2.2rem;
 	font-weight: bold;
 	font-size: 1.2rem;
 	line-height: 1.7rem;
-	color: #ff5c4e;
+	color: var(--orange-dark);
 	text-decoration: none;
-	width: 4.5rem;
 	height: 1.7rem;
 }
-.auth_etc {
+.auth-etc {
 	margin: 0 auto;
 	padding-top: 6.9rem;
 	padding-bottom: 19.4rem;
@@ -101,15 +133,15 @@ button:hover {
 	font-style: normal;
 	font-size: 1.2rem;
 	line-height: 1.4rem;
-	color: #a1a1a1;
+	color: var(--gray02);
 	width: 48rem;
 	height: 1.8rem;
 }
 @media (max-width: 500px) {
-	.auth_text_decoration {
+	.auth-text-decoration {
 		width: 34.3rem;
 	}
-	.auth_etc {
+	.auth-etc {
 		width: 26rem;
 		text-align: center;
 	}

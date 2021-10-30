f052c91af02c43147faa0d2fec053789c8b1a99e
Yujin <yujinyll9148@gmail.com>
2021-10-30 22:29:09 +0900

    배너 디자인 + 반응형 구현 완료

--- src/components/views/home/TheBanner.vue
+++ src/components/views/home/TheBanner.vue
@@ -1,11 +1,14 @@
 <template>
   <section class="banner">
+    <div class="banner-background"></div>
     <div class="contents-container">
       <div class="banner-title-and-btn-container">
         <h1 class="banner__title">
           <span class="banner__title--highlighted">
-            스터디 개설부터 관리까지 -<br />
-            마음에 맞는 스터디 메이트를
+            스터디 개설부터 관리까지 -</span
+          ><br />
+          <span class="banner__title--highlighted"
+            >마음에 맞는 스터디 메이트를
             <span class="banner__title--color">모아모아</span>!
           </span>
         </h1>
@@ -35,12 +38,21 @@ export default {};
 
 <style scoped>
 section {
-  background-color: var(--gray04);
   margin-top: 7rem;
-  padding: 8.3rem 2rem 3.2rem 2rem;
+  padding: 8.3rem 3rem 3.2rem 3rem;
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
+}
+
+.banner-background {
+  background-color: var(--gray06);
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -3;
 }
 
 .contents-container {
@@ -48,6 +60,10 @@ section {
   display: flex;
   justify-content: space-between;
   align-items: center;
+}
+
+.banner-title-and-btn-container {
+  margin-bottom: 4rem;
 }
 
 h1 {
@@ -62,8 +78,32 @@ h1 {
 }
 
 .banner__title--highlighted {
-  background: var(--gray05);
   position: relative;
+}
+
+.banner__title--highlighted:before,
+.banner__title--highlighted:after {
+  content: ' ';
+  height: 2.7rem;
+  position: absolute;
+  bottom: 0.4rem;
+}
+
+.banner__title--highlighted:before {
+  width: calc(100% + 2rem);
+  background: #e6e7ec;
+  z-index: -2;
+  left: -1rem;
+}
+
+.banner__title--highlighted:after {
+  width: calc(100% + 1.4rem);
+  background: var(--gray05);
+  z-index: -1;
+  left: -0.7rem;
+  -webkit-transform: skew(-2deg);
+  -moz-transform: skew(10deg);
+  -o-transform: skew(10deg);
 }
 
 .banner__title--color {
@@ -73,13 +113,91 @@ h1 {
 .banner__btn {
   border: 1px solid var(--orange-dark);
   border-radius: 3.2rem;
-  padding: 1.8rem 2.1rem 1.8rem 1.7rem;
+  padding: 1.6rem 2.1rem 1.6rem 1.7rem;
+  margin-left: -1rem;
+  font-family: Noto Sans KR;
   font-weight: bold;
   font-size: 1.8rem;
   color: var(--orange-dark);
+  background: transparent;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
 }
 
 .btn_icon {
-  margin-left: 0.2rem;
+  margin-left: 0.6rem;
+}
+
+@media (max-width: 1200px) {
+  section {
+    padding: 6.7rem 2rem 3.6rem 2rem;
+  }
+
+  .contents-container {
+    width: auto;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .banner-title-and-btn-container {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .banner__title {
+    font-size: 2rem;
+    text-align: center;
+    line-height: 160%;
+  }
+
+  .banner__title--highlighted:before,
+  .banner__title--highlighted:after {
+    height: 1.64rem;
+    bottom: 0rem;
+  }
+
+  .banner__title--highlighted:before {
+    width: calc(100% + 1rem);
+    left: -0.5rem;
+  }
+
+  .banner__title--highlighted:after {
+    width: calc(100% + 0.4rem);
+    left: -0.2rem;
+  }
+
+  .banner__btn {
+    padding: 1.25rem 1.5rem 1.25rem 1.05rem;
+    margin-left: 0rem;
+    font-size: 1.4rem;
+  }
+
+  .btn_icon {
+    margin-left: 0.3rem;
+    width: 2.4rem;
+    height: 0.7rem;
+  }
+
+  .banner__img {
+    width: 32.9rem;
+    height: 22.3rem;
+  }
+}
+
+@media (max-width: 1024px) {
+  section {
+    margin-top: 5.2rem;
+  }
+}
+
+@media (max-width: 768px) {
+  section {
+    padding-bottom: 1.8rem;
+  }
 }
 </style>

5fb97f70402848a04c2719ee895771ad74981d92
Yujin <yujinyll9148@gmail.com>
2021-12-21 01:27:00 +0900

    [merge] 로그인 코드 post 만들어둔 코드 합침

--- src/api/posts.js
+++ src/api/posts.js
@@ -13,8 +13,39 @@ function fetchPosts() {
     return posts.get('/');
 }
 
-function fetchPostById() {
-    return posts.get('/');
+function fetchPostById(studySeq) {
+    return posts.get('/', {
+        params: {
+          studySeq,
+        },
+      });
 }
+function fetchPostsByStudyType(studyType) {
+    return posts.get('/', {
+      params: {
+        studyType,
+      },
+    });
+  }
+  
+  function fetchPostsByStudyTypeList(studyTypeList) {
+    return posts.get('/', {
+      params: {
+        studyTypeList,
+      },
+    });
+  }
+  
+  function fetchPostsByTitle(title) {
+    return posts.get('/', {
+      params: {
+        title,
+      },
+    });
+  }
+  
 
-export { createPost, updatePost, fetchPosts, fetchPostById };
+  
+  function createReply(reply) {
+    return posts.post('/api/reply', reply)};
+export { createPost, updatePost, fetchPosts, fetchPostById , fetchPostsByStudyType, fetchPostsByStudyTypeList, fetchPostsByTitle};

31e266e5d869919bacc9911e28184f7359be7616
Ujaa <40076944+Ujaa@users.noreply.github.com>
2022-01-09 19:18:19 +0900

    [feat] studymate morebtn 메뉴 구현

--- src/components/views/studyroom/StudyMate.vue
+++ src/components/views/studyroom/StudyMate.vue
@@ -8,39 +8,113 @@
       />
       <div class="nickname">스터디원 2</div>
     </div>
-    <div class="more-btn">
+    <button @click="showMenu = !showMenu" class="more-btn">
       <div></div>
       <div></div>
       <div></div>
-    </div>
-    <div class="menu">
-      <div class="menu-item">프로필 보기</div>
-      <div class="menu-item">스터디 탈퇴</div>
+    </button>
+    <div v-if="showMenu" class="menu">
+      <div class="menu-item">
+        <div class="menu-item--gray">프로필 보기</div>
+      </div>
+      <div class="menu-item"><div class="menu-item--red">스터디 탈퇴</div></div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      showMenu: false,
+    };
+  },
+};
 </script>
 
 <style scoped>
 .study-mate {
+  position: relative;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: 1.4rem 0;
   border-bottom: 0.05rem solid #dedede;
+}
+
+.userinfo {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
 }
 
 .profile-img {
   width: 4.4rem;
   height: 4.4rem;
-  margin-right: 1rem;
 }
 
 .nickname {
   font-weight: bold;
   font-size: 1.4rem;
+}
+
+.more-btn {
+  display: flex;
+  flex-direction: column;
+  gap: 0.2rem;
+  padding: 0.4rem 1rem;
+  background-color: transparent;
+}
+
+.more-btn div {
+  width: 0.4rem;
+  height: 0.4rem;
+  border-radius: 50%;
+  background-color: var(--gray02);
+}
+
+.menu {
+  z-index: 1;
+  background-color: white;
+  width: 10.4rem;
+  position: absolute;
+  right: 2.4rem;
+  top: 5.3rem;
+  border: 1px solid var(--gray03);
+  border-radius: 1rem;
+  box-shadow: 0px 4px 4px rgba(178, 178, 178, 0.25);
+  display: flex;
+  flex-direction: column;
+}
+
+.menu-item:not(:last-child) {
+  border-bottom: 1px solid var(--gray03);
+}
+
+.menu-item {
+  padding: 0.8rem 0;
+}
+
+.menu-item div {
+  font-family: Spoqa Han Sans Neo;
+  font-size: 1.2rem;
+  text-align: center;
+  padding: 0.4rem 0;
+}
+
+.menu-item--gray {
+  color: var(--black);
+}
+.menu-item--gray:hover {
+  background-color: #eff0f3;
+}
+
+.menu-item--red {
+  color: var(--orange-dark);
+}
+
+.menu-item--red:hover {
+  background-color: #ffdedc;
 }
 
 .study-leader {

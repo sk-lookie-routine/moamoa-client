1059d436137be3287b9d66218010e4f92fe0bc09
leeseooo <leeseoooo@naver.com>
2022-01-26 23:58:33 +0900

    feat:마이페이지 route를 통해 받아온 파라미터 userSeq를 통해 유저판별 후 divider show여부 처리 수정

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -9,23 +9,20 @@
 <script>
 import MyPageProfile from '@/components/views/mypage/MyPageProfile.vue';
 import MyPageBottomBox from '@/components/views/mypage/MyPageBottomBox.vue';
-import { getUserByUserSeq } from '@/api/user.js';
 export default {
   components: { MyPageProfile, MyPageBottomBox },
   data() {
     return {
-      user: null,
+      userSeq: null,
       handleDivider: false,
     };
   },
   created() {
+    this.userSeq = this.$route.params.userSeq;
     this.fetchData();
   },
   methods: {
     async fetchData() {
-      const response = await getUserByUserSeq(this.userSeq);
-      this.user = response.data.content[0];
-
       if (this.userSeq == this.$store.state.auth.userSeq) {
         this.handleDivider = true;
       } else {

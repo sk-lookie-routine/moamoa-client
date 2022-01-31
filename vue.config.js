68b155c661568f56c29fdc0a2758de3967895c48
Yujin <yujinyll9148@gmail.com>
2022-01-31 12:43:47 +0900

    [feat] pre render, post 페이지 meta 태그 설정, favicon 및 title 변경

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,33 @@
+const path = require('path');
+const PrerenderSpaPlugin = require('prerender-spa-plugin');
+
+const productionPlugins = [
+  new PrerenderSpaPlugin({
+    staticDir: path.join(__dirname, 'dist'),
+    routes: [
+      '/',
+      '/home',
+      '/signup',
+      '/login',
+      '/post',
+      '/room',
+      '/:notFound(.*)',
+    ],
+    server: {
+      port: 8080,
+    },
+    renderer: new PrerenderSpaPlugin.PuppeteerRenderer({
+      renderAfterElementExists: '#app',
+    }),
+  }),
+];
+
 module.exports = {
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(...productionPlugins);
+    }
+  },
   devServer: {
     overlay: false,
     port: 3000,
